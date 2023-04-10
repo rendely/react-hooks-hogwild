63e86b14d5e85a177e97b6a139e58ad82031adef
matthew rendely <matthew.rendely@gmail.com>
2023-04-09 18:08:58 -0700

    rendering cards with data

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,19 @@
 import React from "react";
 import Nav from "./Nav";
+import Card from "./Card";
 
 import hogs from "../porkers_data";
+console.log('Data:',hogs);
 
 function App() {
 	return (
 		<div className="App">
 			<Nav />
+      <div className="HogList">
+      {hogs.map(hog=>(
+        <Card hog={hog} key={hog.name}/>
+      ))}
+      </div>
 		</div>
 	);
 }
---
+++ src/components/Card.js
@@ -0,0 +1,26 @@
+import React from 'react';
+
+// greased: // false
+// highest medal achieved: // "bronze"
+// image: // "https://raw.githubusercontent.com/learn-co-curriculum/react-hooks-hogwild/master/public/images/babe.jpg"
+// name: // "Babe"
+// specialty: // "Being incredibly cute"
+// weight: // 2
+
+function Card({hog}){
+  return (
+    <div className="Card">
+      <img src={hog.image} alt="hog"/>
+      <div className="description">
+        <div><strong>Name:</strong> {hog.name}</div>
+        <div className="hiddenSection">
+          <div><strong>Greased:</strong> {hog.greased ? "Yes" : "No"}</div>
+          <div><strong>Specialty:</strong> {hog.specialty}</div>
+          <div><strong>Highest medal achieved:</strong> {hog['highest medal achieved']}</div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Card
